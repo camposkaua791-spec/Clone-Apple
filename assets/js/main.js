8a8e5a507fe2e6c3fee125d5cd2307febe9cd354
EJK6NL133 <ejk6nl133@edusesimg.com.br>
2026-09-09 09:35:34 -0300

    primeiro commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,25 @@
-console.log("Clone Apple carregado!");
+console.log("Apple Clone carregado");
 
 const cards = document.querySelectorAll(".card");
 
 cards.forEach(card => {
-    card.addEventListener("mouseover", () => {
-        card.style.transform = "scale(1.03)";
-        card.style.transition = "0.3s";
+
+    card.addEventListener("mouseenter", () => {
+        card.style.transform = "scale(1.02)";
     });
 
-    card.addEventListener("mouseout", () => {
+    card.addEventListener("mouseleave", () => {
         card.style.transform = "scale(1)";
     });
+
+});
+
+window.addEventListener("scroll", () => {
+
+    const evento = document.querySelector(".evento");
+
+    if(evento){
+        evento.style.backgroundPositionY = window.scrollY * 0.3 + "px";
+    }
+
 });
