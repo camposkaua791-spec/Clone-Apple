fc986a70a985e7213d43bf5054c281decf382c04
EJK6NL133 <ejk6nl133@edusesimg.com.br>
2026-09-14 10:42:02 -0300

    primeiro commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,125 @@
-console.log("Apple Clone carregado");
+const links = document.querySelectorAll(".nav-links a");
 
-const cards = document.querySelectorAll(".card");
+links.forEach(function (link) {
 
-cards.forEach(card => {
+    link.addEventListener("click", function () {
 
-    card.addEventListener("mouseenter", () => {
-        card.style.transform = "scale(1.02)";
-    });
+        navLinks.classList.remove("active");
 
-    card.addEventListener("mouseleave", () => {
-        card.style.transform = "scale(1)";
     });
 
 });
 
-window.addEventListener("scroll", () => {
+const topButton = document.getElementById("topButton");
 
-    const evento = document.querySelector(".evento");
+topButton.addEventListener("click", function () {
 
-    if(evento){
-        evento.style.backgroundPositionY = window.scrollY * 0.3 + "px";
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+});
+
+
+const searchButton = document.getElementById("searchButton");
+
+searchButton.addEventListener("click", function () {
+
+    const pesquisa = prompt("O que você está procurando?");
+
+    if (pesquisa) {
+
+        alert(
+            "Você pesquisou por: " + pesquisa
+        );
+
     }
 
 });
 
-function pesquisar(){
-    alert("Barra de pesquisa da Apple");
+
+const cards = document.querySelectorAll(".card");
+
+const observer = new IntersectionObserver(
+
+    function (elementos) {
+
+        elementos.forEach(function (elemento) {
+
+            if (elemento.isIntersecting) {
+
+                elemento.target.style.opacity = "1";
+                elemento.target.style.transform = "translateY(0)";
+
+            }
+
+        });
+
+    },
+
+    {
+        threshold: 0.15
+    }
+
+);
+
+
+cards.forEach(function (card) {
+
+    card.style.opacity = "0";
+    card.style.transform = "translateY(40px)";
+    card.style.transition = "0.7s ease";
+
+    observer.observe(card);
+
+});
+
+const track = document.querySelector(".carousel-track");
+const slides = document.querySelectorAll(".carousel-slide");
+const dots = document.querySelectorAll(".dot");
+
+let slideAtual = 0;
+let transformInicial = ((document.querySelector('.apple-carousel').offsetWidth - 900)/2) - 900
+track.style.transform = `translateX(${transformInicial}px)`;
+
+function mostrarSlide(numero) {
+    slideAtual = numero;
+    const slide = slides[slideAtual];
+
+    track.style.transform = `translateX(${transformInicial - (slideAtual * 900)}px)`;
+    dots.forEach((dot, index) => {
+        dot.classList.toggle("active", index === slideAtual);
+    });
 }
 
-function carrinho(){
-    alert("Sacola da Apple");
+dots.forEach((dot, index) => {
+    dot.addEventListener("click", () => {
+        mostrarSlide(index);
+        clearInterval(mudarImagemInterval)
+        setInterval(criarIntervalo())
+    });
+});
+
+let mudarImagemInterval;
+
+function criarIntervalo(){
+    mudarImagemInterval = setInterval(() => {
+        slideAtual++;
+        if (slideAtual >= slides.length-2){
+            slideAtual = 0;
+        }
+        mostrarSlide(slideAtual);
+    }, 5000);
 }
+
+criarIntervalo()
+
+window.addEventListener('resize', () => {
+    console.log((document.querySelector('.apple-carousel').offsetWidth - 900)/2)
+    transformInicial = ((document.querySelector('.apple-carousel').offsetWidth - 900)/2) - 900
+    track.style.transform = `translateX(${transformInicial}px)`;
+    clearInterval(mudarImagemInterval)
+    setInterval(criarIntervalo())
+})
+
